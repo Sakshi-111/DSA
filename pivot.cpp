efe955acfe27c5c6c6398f8e6663f031f8fec9cb
SAKSHI AGGARWAL <sakshayaggarwal@gmail.com>
2024-03-03 14:44:56 +0530

    sixth

--- pivot.cpp
+++ pivot.cpp
@@ -1,28 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
-int pivottt(int arr[],int size){
-    int start=0;
-    int end=size-1;
-    
-    while(start<end){
-        int mid= start+(end-start)/2;
-         if(arr[mid-1]<arr[mid]  && arr[mid]>arr[mid+1]){
-         return mid;
-         }
-         else if(arr[mid]<arr[0]){
-           end=mid-1;
-        }
-        else{
-            start=mid;
+//pivot using binary search in a sorted rotated array  minimum elmt
 
-        }
-        mid=start+(end-start)/2;
-    }
-    return start;
-    
-}
+// int pivott(int arr[],int size){
+//     int start=0;
+//     int end=size-1;
+//     int mid= start+(end-start)/2;
+//     while(start<end){
+//         if(arr[mid]>arr[0]){
+//             start=mid+1;
+//         }
+//         else{
+//             end=mid;
 
-int main(){
-    int arr[9]={8,9,10,1,2,3,4,5,6};
-    cout<<pivottt(arr,9)<<endl;
-}
+//         }
+//         mid=start+(end-start)/2;
+       
+//     }
+//     return start;
+    
+// }
+// int main(){
+//     int arr[7]={8,9,10,1,5,6,7};
+//     cout<<pivott(arr,7)<<endl;
+// }
+// pivot element is foound in a sorted and then rotated array so that  to find a target value , 
+//binary search can be applied in a single branch whose range is from pivot elemr=ent to n-1 or 
+// from 0 to pivot element 
+//pivot using binary search in a sorted rotated array   maximum elmt
+// /
+// int pivottt(int arr[],int size){
+//     int start=0;
+//     int end=size-1;
+    
+//     while(start<end){
+//         int mid= start+(end-start)/2;
+//          if(arr[mid-1]<arr[mid]  && arr[mid]>arr[mid+1]){
+//          return mid;
+//          }
+//          else if(arr[mid]<arr[0]){
+//            end=mid-1;
+//         }
+//         else{
+//             start=mid;
+
+//         }
+//         mid=start+(end-start)/2;
+//     }
+//     return start;
+    
+// }
+
+// int main(){
+//     int arr[9]={8,9,10,1,2,3,4,5,6};
+//     cout<<pivottt(arr,9)<<endl;
+// }
