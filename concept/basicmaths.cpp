1e772f8bfab8072574b3afb1647d38c757c12db8
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-07-14 12:53:59 +0530

    sieveOfErasthenes

--- concept/basicmaths.cpp
+++ concept/basicmaths.cpp
@@ -1,25 +1,113 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-bool primenumber(){
-    cout<<"enter number to be checked for prime";
-    int n;
-    cin>>n;
-    for(int i=2;i< n;i++){
-        if(n%i==0){
-            return 0;
-        }
+// bool primenumber(){
+//     cout<<"enter number to be checked for prime";
+//     int n;
+//     cin>>n;
+//     for(int i=2;i< n;i++)
+//         if(n%i==0){
+//             return 0;
+//         }
  
-    }
-    return 1;
+//     }
+//     return 1;
 
-}
-int main(){
-    bool p=primenumber();
-    if (p){
-        cout<<"prime";
-    }
-    else{
-        cout<<"not prime";
-    }
+// }
+// int main(){
+//     bool p=primenumber();
+//     if (p){
+//         cout<<"prime";
+//     }
+//     else{
+//         cout<<"not prime";
+//     }
    
-}
+// }
+// class Solution {
+// public:
+//     int countPrimes(int n) {
+//         int count=0;
+//         if(n<=1){
+//             return 0;
+//         }
+//     for(int i=2;i<n;i++){
+//        if( for(int j=2;j<i;j++){
+//             if (i%j==0){
+//                  false;
+//             } 
+//         }
+//          true){
+//             count++;
+//         } 
+//     }
+//     return count;
+//     }
+
+// };
+
+// class Solution {
+// public:
+//     int countPrimes(int n) {
+//         int count = 0;
+//         if (n <= 1) {
+//             return 0;
+//         }
+//         for (int i = 2; i < n; i++) {
+//             bool isPrime = true;
+//             for (int j = 2; j * j <= i; j++) {
+//                 if (i % j == 0) {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+//             if (isPrime) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+// int main() {
+//     Solution solution;
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     int count = solution.countPrimes(n);
+//     cout << "Count of prime numbers less than " << n << ": " << count << endl;
+//     return 0;
+// }
+
+//sieve of erasthenes
+// class Solution {
+//     private:
+    
+    
+//     public:
+
+//     int countprimes(int n){
+//         int count=0;
+//          vector<bool> isPrime(n+1,true);
+//          isPrime[0]=isPrime[1]=false;
+//         for(int i=2;i<n;i++){
+           
+            
+//             if(isPrime[i]){
+//                 count++;
+//                 for(int j=2*i;j<n;j+=i){
+//                     isPrime[j]=0;
+//                 }
+//             }
+
+            
+
+
+//         }
+//         return count;
+//     }
+// };
+// int main(){
+//     Solution solution;
+//      int num= solution.countprimes(3);
+// }
