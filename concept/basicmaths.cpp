638daa36f99732eda375ad8ba163946d0ba64634
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-07-14 15:35:18 +0530

    sorting1

--- concept/basicmaths.cpp
+++ concept/basicmaths.cpp
@@ -111,3 +111,20 @@ using namespace std;
 //     Solution solution;
 //      int num= solution.countprimes(3);
 // }
+
+
+// void selectionSort( vector <int>&arr, int n)
+// {   
+//     for(int i=0;i<n-1;i++){
+//             int mini=i;
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[j]<arr[mini]){
+//                    mini=j;
+//                 }
+
+//             }
+//             swap(arr[mini],arr[i]);
+
+//         }
+     
+// }
