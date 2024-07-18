c99238ab15a657ee87b04df8340cce0ec347418e
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-07-18 21:30:16 +0530

    string

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 void selectionSort(vector<int>nums){
     for(int i=1;i<nums.size();i++){
-        int arr[i]=temp;
-        for(int j=i-1;j>0;j--){
-            if(nums[j-1]>temp){
+        int temp=arr[i];
+        int j=i-1
+        for(;j>0;j--){
+            if(nums[j]>temp){
                 nums[j+1]=nums[j];
                
             }
             else{
                 break;
             }
-            nums[j]=temp;
+            
         }
+        nums[j+1]=temp;
 }
