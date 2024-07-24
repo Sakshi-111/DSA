052a3574085e057fa66708dee7a31c238e9e93b1
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-07-24 23:25:07 +0530

    update

---
+++ 2d_array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int printRowWise(int arr[][4],int row,int col){
+    for(int i=0;i<row;i++){
+    for(int j=0;j<col;j++){
+        cout<<arr[i][j]<< " ";
+}
+cout<<endl;
+
+}
+}
+
+//linear search in 2 d array
+bool linearsearch(int arr[][4],int target ,int row ,int col){
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(arr[i][j]==target){
+return 1;        }
+    }
+
+}
+return 0;}
+
+vector<int> rowsum(int arr[][4],int row,int col){
+    
+    vector<int> SumArr;
+    for(int i=0;i<row;i++){
+        int sum=0;
+        for(int j=0;j<col;j++){
+            sum=sum+ arr[i][j];
+}
+SumArr.push_back(sum);
+    }
+    return SumArr;
+
+}
+int main(){
+// int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+// printRowWise(arr,3,4);
+// int key;
+// cin>>key;
+// if(linearsearch(arr,key,3,4)){
+//     cout<<"found"<<endl;
+
+// }
+// else{
+//     cout<<"not found"<<endl;
+// }
+// vector<int> SumArr=rowsum(arr,3,4);
+// for(int i=0;i<SumArr.size();i++){
+//     cout<<SumArr[i]<<" ";
+// 
+int s= 2&1;    
+cout<<s;
+
+    return 0;
+
+}
+
