e5471051fe6bd1e7168f18b3cfaca14c4694a39c
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-07-27 20:31:51 +0530

    recursion

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+//In a Recursive function three things are mandatory 
+//1. BAse case with a return statement is important to be written in function becase it removes segmentation fault that stops the function from being infinite and using all the memory .
+//2. recursive relation .
+//3. processing part
+
+
+/* BASE CASE 
+   PROCESSING
+   RECURSIVE RELATION
+   tail recursion
+*/
+/* BASE CASE
+   RECURSIVE RELATION
+   PROCESSING
+   head recursion  */
+
 // int factorial(int num){
 //     if(num==0){
 //         return 1;
