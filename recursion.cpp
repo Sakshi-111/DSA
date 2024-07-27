efda0d34067b144fb8ba27063848ed83c30bf7b5
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-07-27 20:03:03 +0530

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// int factorial(int num){
+//     if(num==0){
+//         return 1;
+//     }
+//     return num*factorial(num-1);
+// }
+// int main(){
+//     int n;
+//     cout<<"enter number to be found factorial of ";
+//     cin>>n;
+//     int ans= factorial(n);
+//    cout<<ans;
+//    return 0;
+// }
