6328628fb2df9b206442f501658a326f2a58d25a
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-07-31 23:27:07 +0530

    rec

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,6 @@
-void saynum(int num){
-//     string arr[10]={"one","two","three","four","five","six","seven","eight","nine","ten"};
-//     if(num==0){
-//         return 0 ;}
-//         int digit=num%10;
-//        num=num/10;
-//         saynum(num);
-//          cout<<arr[digit-1];
-        
-// }
-// int main(){
-//     cout<<saynum(345);
+int main(){
+//     int arr[8]={1,5,6,4,3,2};
+//     bubblesort(arr,6);
+//     arrayOutput(arr,6);
 //     return 0;
 // }
