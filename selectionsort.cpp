a1c2bc6e7785e1b895ec4b4f3587a708ee2eb667
Sakshi-111 <sakshayaggarwal@gmail.com>
2024-08-01 01:32:31 +0530

    merge rec

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include<vector>
 
 int arrayOutput(int arr[], int size)
 {
@@ -82,4 +83,57 @@ int arrayOutput(int arr[], int size)
 //     bubblesort(arr,6);
 //     arrayOutput(arr,6);
 //     return 0;
+// }
+// void merge(int arr[],int s,int mid,int e){
+//     vector<int>temp;
+//     int left=s;
+//         int right=mid+1;
+//     while(left<=mid && right<=e){
+        
+//         if(arr[left]<=arr[right]){
+            
+//             temp.push_back(arr[left]);
+//             left++;
+//         }
+//         else{
+//             temp.push_back(arr[right]);
+//             right++;
+
+//         }
+
+//     }
+//     while (left <= mid) {
+//         temp.push_back(arr[left]);
+//         left++;
+//     }
+
+//     while (right <= e) {
+//         temp.push_back(arr[right]);
+//         right++;
+//     }
+
+//     for (int i = s; i <= e; i++) {
+//         arr[i] = temp[i-s];
+//     }
+    
+// }
+
+// void mergesort(int arr[],int s,int e){
+//     if(s>=e){
+//         return;
+//     }
+//     int mid=(s+e)/2;
+//     mergesort(arr,s,mid);
+//     mergesort(arr,mid+1,e);
+//     merge(arr,s,mid,e);
+// }
+// int main(){
+//     int arr[6]={1,4,6,5,32,1};
+//     mergesort(arr,0,5);
+//     for(int i=0;i<6;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     return 0;
+   
+   
 // }
